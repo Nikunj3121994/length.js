e251037203014cadbca9c1f8fe42e9800c362c34
Adam Pałaszyński <35331661+appalaszynski@users.noreply.github.com>
2018-04-23 14:45:36 +0200

    Use the meter as default unit, add new units
    Newly supported units are decameter, hectometer, nautical mile and parsec.

--- src/lib/units.js
+++ src/lib/units.js
@@ -1,19 +1,23 @@
-// This object stores information about dependences between units and centimeter.
+// This object stores information about dependences between units and meter.
 var standardUnitDependences = {
   pm: Math.pow(10, -12),
-  nm: Math.pow(10, -7),
-  um: Math.pow(10, -4),
-  mm: 0.1,
-  cm: 1,
-  dm: 10,
-  m: Math.pow(10, 2),
-  km: Math.pow(10, 5),
-  in: 2.54,
-  ft: 30.48,
-  yd: 91.44,
-  mi: 160934.4,
-  au: 14959787069100,
-  ly: 946073047258080000,
+  nm: Math.pow(10, -9),
+  um: Math.pow(10, -6),
+  mm: Math.pow(10, -3),
+  cm: Math.pow(10, -2),
+  dm: Math.pow(10, -1),
+  m: 1,
+  dam: Math.pow(10, 1),
+  hm: Math.pow(10, 2),
+  km: Math.pow(10, 3),
+  nmi: 1852,
+  in: 0.0254,
+  ft: 0.3048,
+  yd: 0.9144,
+  mi: 1609.344,
+  au: 149597870700,
+  ly: 9460730472580800,
+  pc: (648000 / Math.PI) * 149597870700,
 };
 
 // Array of currently supported units.

42526f18abd09aea692fe683b245d632180bc80a
Adam <35331661+appalaszynski@users.noreply.github.com>
2018-06-09 17:00:45 +0200

    Build 1.0.0

--- src/length.js
+++ src/length.js
@@ -5,7 +5,7 @@ import { to, add, getString, getUnit, getValue, toPrecision } from './lib/functi
 var proto = Length.prototype;
 
 // Add current version number to Length object prototype.
-proto.version = '0.1.0';
+proto.version = '1.0.0';
 
 // Add functions to Length object prototype.
 proto.to = to;

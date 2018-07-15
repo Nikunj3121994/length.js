26e691a5d9321d611b48c40c94aeea874e9398e8
appalaszynski <35331661+appalaszynski@users.noreply.github.com>
2018-07-15 11:17:45 +0200

    Fix 'build' task success message

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -1,6 +1,6 @@
-const gulp   = require('gulp');
+const gulp = require('gulp');
 const rollup = require('rollup').rollup;
-const fs     = require('fs');
+const fs = require('fs');
 
 gulp.task('build', function() {
   const rollupOpts = {
@@ -32,7 +32,7 @@ gulp.task('build', function() {
           // Save fixed code.
           fs.writeFile('./length.js', fixed, (err) => {
             if (err) throw err;
-            console.log("\x1b[1m\x1b[32m", '\n    length.js generated correctly!\n');
+            console.log("\x1b[1m\x1b[32m%s\x1b[0m", '\n    length.js generated correctly!\n');
           });
         });
       })

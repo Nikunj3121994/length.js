73ae1ae9d2d5ee1095883d7ac67c8b91f19e680a
appalaszynski <35331661+appalaszynski@users.noreply.github.com>
2018-07-15 11:23:32 +0200

    Change the way of version bumping
    Thanks to 'prompt' npm package user can easily pass new version number.
    Next, script file sets environment variable and execute Gulp task.

--- gulp/tasks/bump.js
+++ gulp/tasks/bump.js
@@ -1,11 +1,6 @@
-const gulp            = require('gulp');
-var { versionNumber } = require('../config');
+const gulp = require('gulp');
 
 gulp.task('bump', function (done) {
-  // Check if version number provided by user matches X.X.X pattern.
-  if (!versionNumber || versionNumber.split('.').length !== 3) {
-    throw Error('\n\n    Incorrect version number. Use:\n    $ npm run bump -- 1.2.3\n    or\n    $ gulp bump --versionNumber 1.2.3\n');
-  }
   return gulp.series(['stringReplacePackageJson',
     'stringReplacePackageLock',
     'stringReplaceLengthJs',
